af4620d3137a229ffeb0e04b8cc4e7cc0811b1f4
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-27 19:13:55 +0700

    Initial Commit

---
+++ Pull to Refresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pull to Refresh
+//
+//  Created by Abdul R. Arraisi on 27/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pull to Refresh/Pull_to_RefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pull_to_RefreshApp.swift
+//  Pull to Refresh
+//
+//  Created by Abdul R. Arraisi on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Pull_to_RefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
